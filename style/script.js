e4f9db7f43ea854014b6c0bb3276f567ddfc38f8
NathansMaia <nathanaeldasilvamaia@gmail.com>
2024-07-10 14:06:28 -0300

    Primeira-versao

---
+++ style/script.js
@@ -0,0 +1,4 @@
+document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    document.querySelector('.container').classList.toggle('dark-mode');
+});
